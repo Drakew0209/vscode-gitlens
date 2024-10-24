ae78b11561d81b6dcaab0ef219ab470af9332022
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2024-10-24 18:49:23 +0200

    Uses `slug` to convert issue title and id to branch name (#3621)

--- src/plus/startWork/startWork.ts
+++ src/plus/startWork/startWork.ts
@@ -1,3 +1,4 @@
+import slug from 'slug';
 import type { QuickInputButton, QuickPick } from 'vscode';
 import { ThemeIcon, Uri } from 'vscode';
 import type {
@@ -168,7 +169,7 @@ export class StartWorkCommand extends QuickCommand<State> {
 								state: {
 									subcommand: 'create',
 									repo: undefined,
-									name: `${state.item.item.issue.id}-${state.item.item.issue.title}`,
+									name: slug(`${state.item.item.issue.id}-${state.item.item.issue.title}`),
 									suggestNameOnly: true,
 								},
 							},
